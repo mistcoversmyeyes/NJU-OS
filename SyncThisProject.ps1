6fb98606aa3046c25e070e01787ff1bf2b24c94a
yuming <mingjiangyu1@qq.com>
2025-02-06 14:01:24 +0800

    添加了一个powershell 脚本，用于将项目同步到ondrive文件夹下

---
+++ SyncThisProject.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    执行SyncToy里面用于同步这个文件夹的同步配置
+#>
+
+#定义命令行程序绝对路径
+$syncToyCmdPath = "C:\Program Files\SyncToy 2.1\SyncToyCmd.exe"
+
+#设定同步组的名称
+$syncGroupName = "NJU-OS文件夹同步"
+
+#构建命令文本
+$command = "&`"$syncToyCmdPath`" -R$syncGroupName"
+
+#执行命令
+Invoke-Expression $command 
